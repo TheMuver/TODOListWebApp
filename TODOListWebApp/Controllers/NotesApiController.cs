a688c1414d97ba8b2ef14b142fab5f5fcd773fcc
Muver <via.zyn@ya.ru>
2021-06-02 23:38:47 +0300

    get login from coockies?

--- TODOListWebApp/Controllers/NotesApiController.cs
+++ TODOListWebApp/Controllers/NotesApiController.cs
@@ -1,4 +1,8 @@
+using System.Security.Claims;
+using System.Runtime.Intrinsics.X86;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +13,12 @@ namespace TODOListWebApp.Controllers
     [Authorize]
     public class NotesApiController : ControllerBase
     {
-        
+        [HttpGet]
+        [Route("getallnotes")]
+        public List<Note> GetAllNotes() 
+        {
+            Console.WriteLine(HttpContext.User.Claims.First(c => true).Value);
+            return new List<Note>();
+        }
     }
 }
