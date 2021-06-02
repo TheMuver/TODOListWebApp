b1120e608766cee790271c112bfb24602b135a04
Muver <via.zyn@ya.ru>
2021-06-02 19:40:23 +0300

    some changes in login.html

--- TODOListWebApp/Controllers/AuthApiController.cs
+++ TODOListWebApp/Controllers/AuthApiController.cs
@@ -22,21 +22,19 @@ namespace TODOListWebApp.Controllers
 
         [HttpPost]
         [Route("auth")]
-        public IActionResult Auth() 
+        public IActionResult Auth([FromBody] User user) 
         {
-            User user = new User("lol", "lol");
-            List<Claim> claims = new List<Claim>() { new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login) };
-            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
-            return Ok("notes");
-            // if (_users.IsCorrectData(user.ToDTO())) 
-            // {
-
-            // }
-            // else 
-            // {
-            //     return StatusCode(StatusCodes.Status404NotFound);
-            // }
+            if (user.Login.Equals("lol@ya.ru") && user.Login.Equals("lol")) 
+            {
+                List<Claim> claims = new List<Claim>() { new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login) };
+                ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+                return Ok("notes");
+            }
+            else 
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
         }
     }
 }
--- TODOListWebApp/Models/User.cs
+++ TODOListWebApp/Models/User.cs
@@ -6,9 +6,11 @@ namespace TODOListWebApp
 {
     public class User
     {
+        [Required(ErrorMessage = "Впишите адрес электронной почты")]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
         public string Login { get; set; }
 
+        [Required(ErrorMessage = "Впишите пароль")]
         public string Password { get; set; }
 
         public User() { }
